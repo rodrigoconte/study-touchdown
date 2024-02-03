2649e470dc152fdace18c7a8d9855bb95014a1ea
Rodrigo Conte <rodrigo.conte14@gmail.com>
2024-02-02 21:41:04 -0400

    feat: Including some groups and constants

---
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Rodrigo Conte on 02/02/24.
+//
+
+import SwiftUI
+
+// MARK: - DATA
+// MARK: - COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// MARK: - LAYOUT
+// MARK: - UX
+// MARK: - API
+// MARK: - IMAGE
+// MARK: - FONT
+// MARK: - STRING
+// MARK: - MISC
+
