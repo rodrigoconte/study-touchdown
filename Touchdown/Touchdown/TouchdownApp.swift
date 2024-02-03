edc12713136bce054df0d9732d7bc5de6561be46
Rodrigo Conte <rodrigo.conte14@gmail.com>
2024-02-02 20:16:23 -0400

    feat: Initial setup of the app

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Rodrigo Conte on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
